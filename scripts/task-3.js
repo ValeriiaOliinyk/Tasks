67b28c10084a78eaf2b84e927ec80629ee21afb4
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-05-03 18:56:24 +0300

    added map to the function at the task number 3

--- scripts/task-3.js
+++ scripts/task-3.js
@@ -7,6 +7,6 @@ import users from './users.js';
 console.log('Task 3');
 
 const getUsersWithGender = (arr, gender) =>
-  arr.filter(user => user.gender === gender);
+  arr.filter(user => user.gender === gender).map(user => user.name);
 
 console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
